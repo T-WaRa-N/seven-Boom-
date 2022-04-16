0e8661e4a4b466aca3fa184c6f323120e2833a44
Wandile Nondabula <NNDTHA005@myuct.ac.za>
2022-04-16 12:09:52 +0200

    Number that are containing number 7 also take care of

--- sevenBoom.js
+++ sevenBoom.js
@@ -1,15 +1,28 @@
 //function that return boom! there is a number
 // Seven in an array.
 
-let array = [5, 7, 8, 11]
+let array = [5, 97, 8, 11]
 
 let sevenBoom = (arrayArgument) => {
+
    let seven = "absent"
+
    for(let i = 0; i < arrayArgument.length; i++){
-       if(arrayArgument[i] == 7){
-          seven = "present"
+       let arrayElem = arrayArgument[i].toString().split('')
+       
+       for(let j = 0; j < arrayElem.length; j++){
+
+            if(arrayElem[j] == 7){
+                seven = "present"
+            }
+
         }
-   }
+
+        if(seven == "present"){
+            break
+        }
+       
+    }
     return seven == "present"? "Boom!": "None of the items contains no. 7 within them"
 }
 
--- sevenBoom.test.js
+++ sevenBoom.test.js
@@ -5,3 +5,13 @@ test("tests if is there a number seven in the array",() => {
     let array = [5, 0, 8, 11]
     expect(sevenBoom(array)).toBe("None of the items contains no. 7 within them")
 })
+
+test("tests if is there a number seven in the array",() => {
+    let array = [5, 7, 8, 11]
+    expect(sevenBoom(array)).toBe("Boom!")
+})
+
+test("tests if is there a number seven in the array",() => {
+    let array = [5, 9, 8, 117]
+    expect(sevenBoom(array)).toBe("Boom!")
+})
